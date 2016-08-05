412a7bf2f7561da71023862ce5c7ea98a8eac85d
Julien Enselme <julien.enselme@ioda.net>
2016-08-05 10:04:42 +0200

    Add GNU AGPLv3 header to source files

--- help/help.js
+++ help/help.js
@@ -1,3 +1,26 @@
+/*
+* geo-infra Scripts and templates to create and manage geoportals
+* Copyright (c) 2015-2016, sigeom sa
+* Copyright (c) 2015-2016, Ioda-Net Sàrl
+*
+* Contact : contact (at)  geoportal (dot) xyz
+* Repository : https://github.com/ioda-net/geo-infra
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 (function (angular) {
     'use strict';
 
