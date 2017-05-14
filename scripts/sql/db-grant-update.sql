42dc9b8d93f3eaf7da19a4abbf01786809373d73
Bruno Friedmann <bruno@ioda-net.ch>
2017-05-14 10:33:34 +0200

    New db grant procedure
    - Support addition of any schema name and give automatically
      Read Only rights to our additionnals roles.
    - Ease the use of the procedure outside of manuel.
      Replacement happen at only one place now.

--- scripts/sql/db-grant-update.sql
+++ scripts/sql/db-grant-update.sql
@@ -1,98 +1,64 @@
 --
 -- run this script with DEFAULT_DB_OWNER
 --
+DO $$
+-- roles[1] has to always be geo_dba, [2] geo_api
+DECLARE roles varchar[] := array['DEFAULT_DB_OWNER','DEFAULT_DB_API_ROLE','DEFAULT_DB_SEARCH_ROLE','DEFAULT_DB_MAPSERVER_ROLE'];
+DECLARE ro varchar;
+DECLARE r record;
+DECLARE o record;
+BEGIN
+    FOR r IN select schemaname from pg_catalog.pg_tables
+              where schemaname in ('api3','edit','features','search','osm','public','userdata','topology')
+              group by schemaname
+              order by schemaname
+    LOOP
+          -- Give back all rights to owner
+          EXECUTE '
+            GRANT ALL ON SCHEMA ' || quote_ident(r.schemaname) || ' TO ' || quote_ident(roles[1]) || ';
+            GRANT ALL ON ALL TABLES IN SCHEMA '  || quote_ident(r.schemaname) || ' TO ' || quote_ident(roles[1]) || ';
+            GRANT ALL ON ALL FUNCTIONS IN SCHEMA '  || quote_ident(r.schemaname) || ' TO '|| quote_ident(roles[1]) || ';
+          ';
+        
+        IF r.schemaname = 'public' THEN
+          -- Only owner as full rights here
+          -- Could be redondant with previous execution.
+          EXECUTE '
+            GRANT SELECT, REFERENCES, TRIGGER ON TABLE '  || quote_ident(r.schemaname) || '.spatial_ref_sys TO '|| quote_ident(roles[1]) || ';
+            GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE ' || quote_ident(r.schemaname) || '.geography_columns TO '|| quote_ident(roles[1]) || ';
+            GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE ' || quote_ident(r.schemaname) || '.geometry_columns TO ' || quote_ident(roles[1]) || ';
+            GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE ' || quote_ident(r.schemaname) || '.raster_columns TO '   || quote_ident(roles[1]) || ';
+            GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE ' || quote_ident(r.schemaname) || '.raster_overviews TO ' || quote_ident(roles[1]) || ';
+          ';
+        END IF;
 
+        -- Make all other read only
+        FOREACH ro IN ARRAY roles
+        LOOP
+          IF ro != roles[1] THEN
+          -- RAISE NOTICE 'Owner is now %', ro;
+           -- Remove any rights from other than owner
+           -- And redistribute correctly rights
+          EXECUTE '
+            REVOKE ALL ON SCHEMA ' || quote_ident(r.schemaname) || ' FROM ' || quote_ident(ro) || ';
+            REVOKE ALL ON ALL TABLES IN SCHEMA '  || quote_ident(r.schemaname) || ' FROM '|| quote_ident(ro) || ';
+            REVOKE ALL ON ALL FUNCTIONS IN SCHEMA '  || quote_ident(r.schemaname) || ' FROM '|| quote_ident(ro) || ';
+            GRANT USAGE ON SCHEMA '  || quote_ident(r.schemaname) || ' TO '|| quote_ident(ro) || ';
+            GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA '  || quote_ident(r.schemaname) || ' TO '|| quote_ident(ro) || ';
+            GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA '  || quote_ident(r.schemaname) || ' TO '|| quote_ident(ro) || ';
+          ';
+          END IF;
 
- GRANT ALL ON SCHEMA userdata TO DEFAULT_DB_OWNER;
- GRANT ALL ON SCHEMA api3 TO DEFAULT_DB_OWNER;
- GRANT ALL ON SCHEMA features TO DEFAULT_DB_OWNER;
- GRANT ALL ON SCHEMA search TO DEFAULT_DB_OWNER;
--- Fix the error of ERROR:  grant options cannot be granted back to your own grantor
--- GRANT ALL ON SCHEMA public TO DEFAULT_DB_OWNER WITH GRANT OPTION;
--- GRANT ALL ON SCHEMA topology TO DEFAULT_DB_OWNER WITH GRANT OPTION;
- GRANT ALL ON SCHEMA public TO DEFAULT_DB_OWNER;
- GRANT ALL ON SCHEMA topology TO DEFAULT_DB_OWNER;
- GRANT SELECT, REFERENCES, TRIGGER ON TABLE public.spatial_ref_sys TO DEFAULT_DB_OWNER;
- GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE public.geography_columns TO DEFAULT_DB_OWNER;
- GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE public.geometry_columns TO DEFAULT_DB_OWNER;
- GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE public.raster_columns TO DEFAULT_DB_OWNER;
- GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE public.raster_overviews TO DEFAULT_DB_OWNER;
+          -- Special case for api schema where geo_api need control on some tables
+          IF r.schemaname = 'api3' AND ro = roles[2] THEN
+          EXECUTE '
+            GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE '  || quote_ident(r.schemaname) || '.url_shortener TO ' || quote_ident(roles[2]) || ';
+            GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE '  || quote_ident(r.schemaname) || '.files TO ' || quote_ident(roles[2]) || ';
+          ';
+          END IF;
+          
+        END LOOP; -- End roles
+        
+    END LOOP; -- End schemas
+END$$;
 
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA topology TO DEFAULT_DB_OWNER;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO DEFAULT_DB_OWNER;
-
-
--- mapserver user
--- Read Only access on public & userdata
-
- REVOKE ALL ON SCHEMA public FROM DEFAULT_DB_MAPSERVER_ROLE;
- GRANT USAGE ON SCHEMA public TO DEFAULT_DB_MAPSERVER_ROLE;
- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM DEFAULT_DB_MAPSERVER_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA public TO DEFAULT_DB_MAPSERVER_ROLE;
- REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM DEFAULT_DB_MAPSERVER_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO DEFAULT_DB_MAPSERVER_ROLE;
-
- REVOKE ALL ON SCHEMA topology FROM DEFAULT_DB_MAPSERVER_ROLE;
- GRANT USAGE ON SCHEMA topology TO DEFAULT_DB_MAPSERVER_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA topology TO DEFAULT_DB_MAPSERVER_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO DEFAULT_DB_MAPSERVER_ROLE;
-
- REVOKE ALL ON SCHEMA userdata FROM DEFAULT_DB_MAPSERVER_ROLE;
- GRANT USAGE ON SCHEMA userdata TO DEFAULT_DB_MAPSERVER_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA userdata TO DEFAULT_DB_MAPSERVER_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA userdata TO DEFAULT_DB_MAPSERVER_ROLE;
-
-
--- Sphinx searchd user
--- Access Read Only everything
-
- REVOKE ALL ON SCHEMA public FROM DEFAULT_DB_SEARCH_ROLE;
- GRANT USAGE ON SCHEMA public TO DEFAULT_DB_SEARCH_ROLE;
- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM DEFAULT_DB_SEARCH_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA public TO DEFAULT_DB_SEARCH_ROLE;
- REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM DEFAULT_DB_SEARCH_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO DEFAULT_DB_SEARCH_ROLE;
-
- REVOKE ALL ON SCHEMA topology FROM DEFAULT_DB_SEARCH_ROLE;
- GRANT USAGE ON SCHEMA topology TO DEFAULT_DB_SEARCH_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA topology TO DEFAULT_DB_SEARCH_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO DEFAULT_DB_SEARCH_ROLE;
-
- REVOKE ALL ON SCHEMA search FROM DEFAULT_DB_SEARCH_ROLE;
- GRANT USAGE ON SCHEMA search TO DEFAULT_DB_SEARCH_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA search TO DEFAULT_DB_SEARCH_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA search TO DEFAULT_DB_SEARCH_ROLE;
-
- REVOKE ALL ON SCHEMA userdata FROM DEFAULT_DB_SEARCH_ROLE;
- GRANT USAGE ON SCHEMA userdata TO DEFAULT_DB_SEARCH_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA userdata TO DEFAULT_DB_SEARCH_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA userdata TO DEFAULT_DB_SEARCH_ROLE;
-
-
--- API3 user
--- Access Read Only api3 + features + userdata ro
-
- REVOKE ALL ON SCHEMA public FROM DEFAULT_DB_API_ROLE;
- GRANT USAGE ON SCHEMA public TO DEFAULT_DB_API_ROLE;
- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM DEFAULT_DB_API_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA public TO DEFAULT_DB_API_ROLE;
- REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM DEFAULT_DB_API_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO DEFAULT_DB_API_ROLE;
-
- REVOKE ALL ON SCHEMA topology FROM DEFAULT_DB_API_ROLE;
- GRANT USAGE ON SCHEMA topology TO DEFAULT_DB_API_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA topology TO DEFAULT_DB_API_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA topology TO DEFAULT_DB_API_ROLE;
-
- REVOKE ALL ON SCHEMA api3 FROM DEFAULT_DB_API_ROLE;
- GRANT USAGE ON SCHEMA api3 TO DEFAULT_DB_API_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA api3 TO DEFAULT_DB_API_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA api3 TO DEFAULT_DB_API_ROLE;
-
- GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE api3.url_shortener TO DEFAULT_DB_API_ROLE;
- GRANT SELECT, UPDATE, INSERT, DELETE, REFERENCES, TRIGGER ON TABLE api3.files TO DEFAULT_DB_API_ROLE;
-
- REVOKE ALL ON SCHEMA userdata FROM DEFAULT_DB_API_ROLE;
- GRANT USAGE ON SCHEMA userdata TO DEFAULT_DB_API_ROLE;
- GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA userdata TO DEFAULT_DB_API_ROLE;
- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA userdata TO DEFAULT_DB_API_ROLE;
